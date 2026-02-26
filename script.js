9eead32906c2e261ac4781acab01ce17593a1b9e
guilherme0226 <guilherme.2956@aluno.pr.senac.br>
2026-02-26 17:48:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//Pedir Idade
+//Comparar a idade do usuario com a lei
+//Resposta
+
+let idadeUser = prompt("Digite sua idade");
+let estadoUser = prompt("Digite seu estado:").toUpperCase();
+
+if (idadeUser >=18 && estadoUser === "PR") {
+    document.getElementById1("lascou").innerText = "voce esta apto a iniciar!";
+    document.getElementById2("Vish").innerText = "Parabens";
+    document.getElementById3("Correr").innerText = "Vá até o Detran mais próximo";
+    document.getElementById4("carro").src ="https://static.vecteezy.com/ti/fotos-gratis/p2/47267148-gato-sorridente-mostrando-dentes-felino-retrato-foto.jpg";
+} else {
+    document.getElementById5("Desculpe").innerText = "!";
+    document.getElementById6("").innerText = "";
+    document.getElementById7("Error ").innerText = "!";
+    document.getElementById("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQtpFuIFOXJxSR3b6P8vX5lEPjf7x-LDzH8g&s").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQtpFuIFOXJxSR3b6P8vX5lEPjf7x-LDzH8g&s";
+}
+   
+
+
+
+
